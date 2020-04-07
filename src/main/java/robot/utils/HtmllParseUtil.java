05203580f86c719c0347415b69e65aee45dbbd3c
tengjun <870409697@qq.com>
2020-04-07 13:25:23 +0800

    first

---
+++ src/main/java/robot/utils/HtmllParseUtil.java
@@ -0,0 +1,33 @@
+package robot.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class HtmllParseUtil {
+//    public List<Object> parseJD(String keywords) throws IOException {
+//        //获取请求 https://search.jd.com/Search?keyword=java
+//        //前提需要联网 ajax不能获取到
+//        String url = "https://search.jd.com/Search?keyword=java";
+//        //解析网页 Jsoup返回的Document就是Document对象
+//        Document document = Jsoup.parse(new URL(url),30000);
+//        //所有你在js中可以使用的方法 这里都能用
+//        Element element = document.getElementById("J_searchWrap");
+//        Elements elements = element.getElementsByClass("li");
+//        for(Element e : elements){
+//
+//        }
+//    }
+
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
